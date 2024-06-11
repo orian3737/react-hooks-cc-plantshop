939170b5d6a8c26096dd98b70a3289831ccedcbf
Orian shillz <91493524+orian3737@users.noreply.github.com>
2024-06-11 09:47:43 -0400

    new

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -17,7 +17,7 @@ function PlantPage() {
     fetch("http://localhost:6001/plants", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "Application/JSON"
       },
       body: JSON.stringify(newPlant)
     })
